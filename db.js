6e6538bbaea4caf61b0ff57dc7bb313d1053d532
Derrique B <derriquebaluyut@gmail.com>
2021-10-20 12:27:44 -0700

    expose env variables

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const knex = require('knex')({
   version: '7.2',
   connection: {
     host: 'ec2-18-215-96-54.compute-1.amazonaws.com',
-    user: process.env.DB_USER,
+    user: 'lcorrufwwnrecq',
     password:
       '3ac5199330b8962cd3d08c1e1173c0a93e4150431c48812e37c5e8d9f84e47da',
-    database: process.env.DB_NAME,
+    database: 'daicfrugl8kvoo',
   },
 })
 
