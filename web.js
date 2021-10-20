f52c6a598876b6035b1066cb9164c6d624ccefc2
Derrique B <derriquebaluyut@gmail.com>
2021-10-20 13:35:28 -0700

    test

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ const express = require('express')
 const jwtMiddleware = require('express-jwt')
 const jwtToken = require('jsonwebtoken')
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.DB_PORT || 5000
 // const exampleRoutes = require("./routes/example");
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -21,7 +21,6 @@ const recommendationsRoutes = require('./routes/recommendations')
 
 app.get('/', (req, res) => {
   res.send('hello world')
-  console.log(conn)
 })
 
 app.use('/api', adminsRoutes)
