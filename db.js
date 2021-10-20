21301a5e64a80c05ccb3afaa76f701319ef8fce7
Derrique B <derriquebaluyut@gmail.com>
2021-10-20 12:44:02 -0700

    change to process.env values

--- db.js
+++ db.js
@@ -2,11 +2,10 @@ const knex = require('knex')({
   client: 'pg',
   version: '7.2',
   connection: {
-    host: 'ec2-18-215-96-54.compute-1.amazonaws.com',
-    user: 'lcorrufwwnrecq',
-    password:
-      '3ac5199330b8962cd3d08c1e1173c0a93e4150431c48812e37c5e8d9f84e47da',
-    database: 'daicfrugl8kvoo',
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
   },
 })
 
